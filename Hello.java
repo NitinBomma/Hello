5f21098fc970e3fd4e00662195561a3840ef5ce7
NitinBomma <52826370+NitinBomma@users.noreply.github.com>
2021-05-24 12:22:47 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
 public static void main(String args[]){
 System.out.println("Hello World");
-System.out.println("I am in Jenkins git job");
+System.out.println("I am in  git job");
 }
 }
